9a682bcd665fbdf6bc919709ecaeb1f162c79463
goldyl <vitaliyfnaf@gmail.com>
2025-05-09 20:56:31 +0300

    some stuff for chat

---
+++ src/main/java/messanger/app/simplechat/ChatController.java
@@ -0,0 +1,16 @@
+package messanger.app.simplechat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ChatController {
+
+    @FXML
+    public ListView messageListView;
+    public TextField messageField;
+    public Button sendButton;
+    public Button logoutButton;
+
+}
