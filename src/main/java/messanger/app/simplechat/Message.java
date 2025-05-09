e82698749f3d24164dd5b28cc44ba7dfde9bc879
goldyl <vitaliyfnaf@gmail.com>
2025-05-09 21:09:12 +0300

    some reworks

--- src/main/java/messanger/app/simplechat/Message.java
+++ src/main/java/messanger/app/simplechat/Message.java
@@ -4,9 +4,9 @@ package messanger.app.simplechat;
 public class Message {
     private String message;
     private User user;
-    private String time;
+    private final String time;
 
-    public Message(String message, User user,String time){
+    public Message(String message, User user, String time) {
         this.message = message;
         this.user = user;
         this.time = time;
--- src/main/java/messanger/app/simplechat/User.java
+++ src/main/java/messanger/app/simplechat/User.java
@@ -3,11 +3,30 @@ package messanger.app.simplechat;
 public class User {
     private String userName;
     private String password;
-    private String Email;
+    private final String Email;
 
-    public User(String userName,String password,String Email){
+    public User(String userName, String password, String Email) {
         this.userName = userName;
         this.password = password;
         this.Email = Email;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void switchPassword(String oldPassword, String newPassword) {
+        if (oldPassword.equals(this.password))
+            this.password = newPassword;
+        else
+            System.out.println("Incorrect password");
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
